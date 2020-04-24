6ee135f586902cb8eb14b476b09f85fc90d160eb
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-04-24 11:58:30 -0700

    create list view and detail views

---
+++ Newsstand/Controller/ArticlesListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ArticlesListViewController.swift
+//  Newsstand
+//
+//  Created by Bo on 4/24/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArticlesListViewController: UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+    }
+}
---
+++ Newsstand/Controller/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Newsstand
+//
+//  Created by Bo on 4/24/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
+    }
+}
